070f128982bef45809cb257881f1f02c3a8845b7
tokinotoki <999r.like.moto@gmail.com>
2012-03-27 05:24:47 +0900

    戻しました

--- usr/clock.lua
+++ usr/clock.lua
@@ -1,21 +1,9 @@
 interval = 0.5
 out = "front"
 
-startCom = "start"
-stopCom = "stop"
-
-input = read()
-
 while true do
-if input == startCom then
-	while true do
-  	rs.setOutput( (out),true)
-  	sleep(interval)
-  	rs.setOutput( (out),false)
-  	sleep(interval)
-	end
-	
-	else
-	rs.setOutput( (out),false)
-end
+rs.setOutput( (out),true)
+sleep(interval)
+rs.setOutput( (out),false)
+sleep(interval)
 end
