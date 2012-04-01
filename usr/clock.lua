663b71d1d26ec15386f590e5571d574b42450b8c
tokinotoki <999r.like.moto@gmail.com>
2012-04-01 16:19:43 +0900

    レッドストーン入力があると起動するように修正

--- usr/clock.lua
+++ usr/clock.lua
@@ -1,9 +1,13 @@
 interval = 0.5
 out = "front"
+switchIn = "left"
 
+while true do
+if rs.getInput((switchIn),true)then
 while true do
 rs.setOutput( (out),true)
 sleep(interval)
 rs.setOutput( (out),false)
 sleep(interval)
+end
 end
